ac7e2c94cd5011400f9f43ec5d1e23e21121050a
Bartek Tułodziecki <bartek.tulodziecki@gmail.com>
2025-06-04 14:04:49 +0200

    added assets to app clip

--- ios/RunnerAppClip/AppDelegate.swift
+++ ios/RunnerAppClip/AppDelegate.swift
@@ -10,11 +10,12 @@ import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-//    lazy var flutterEngine = FlutterEngine(name: "my_flutter_engine")
+    lazy var flutterEngine = FlutterEngine(name: "RunnerAppClipEngine")
 
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        flutterEngine.run(withEntrypoint: "appClipMain", libraryURI: "main_app_clip.dart");
-        GeneratedPluginRegistrant.register(with: self)
+        flutterEngine.run(withEntrypoint: "appClipMain", libraryURI: "main_app_clip.dart");
+        GeneratedPluginRegistrant.register(with: self.flutterEngine)
+//        GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
